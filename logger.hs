5243a40104e0647836aa3212ec68af91584a0f6a
Wander Hillen <wjw.hillen@gmail.com>
2015-11-07 18:36:31 +0100

    klaarmaken voor eerste deploy

--- logger.hs
+++ logger.hs
@@ -11,21 +11,31 @@ main = withGPIO . withSPI $ do --setup some things
     setDataModeSPI (False,False) 
     measurement <- doMeasurement 
     waitForConnection
-    battery <- readBattery
+    battery <- return 100 --readBattery
     temperature <- readTemperature
-    shouldStayAlive <- postToNivix $ SENSOR battery temperature
+    shouldStayAlive <- postToNivix $ STUW battery temperature
     if shouldStayAlive then return () else shutdown
     
---Posts een request naar
+--Posts een request naar Nivix
 postToNivix :: Event -> IO Bool
 postToNivix evt = handle (\(e :: HttpException) -> return false) $ do --als het faalt, dan sowieso weer gaan slapen
     req <- makePostRequest $ encode evt
     Response bs <- withManager $ \manager -> httpLbs req manager
     return $ bs == "stayalive"
-    
-shutdown = shell "sudo shutdown -h now"
 
 --parseURL werkt in een monad m die in de failure class zit, een voorbeeld van die class is good old Maybe.
 makePostRequest :: BSL.ByteString -> Request
 makePostRequest payload = initreq { method = POST, requestBody = RequestBodyLBS payload } --encode geeft een lazy BS
-    where (Just initreq) = parseUrl "http://192.168.178.50:3000" --poort 3000 op de vaste pi.
+    where   (Just initreq) = parseUrl "http://192.168.178.20:3000" --poort 3000 op de laptop.
+
+--shuts down, tenzij pin11 hoog is.
+shutdown :: IO ()
+shutdown = do
+    setPinFunction Pin11 Input
+    preventShutdown <- readPin Pin11 --voor debuggen is het handig om ook als er geen server is de pi te laten leven
+    if preventShutdown then return () else spawnCommand "sudo shutdown -h now"
+    
+--aeson werkt niet op de pi omdat je geen TH kan gebruiken. dit is natuurlijk een nogal brakke vervanger, maar je moet wat...
+encode (STUW bat temp) = BSL.concat ["{\"tag\":\"STUW\",\"contents\":[", BSL.pack . show $ bat, ",", BSL.pack . show $ temp, "]}\""]
+
+data Event = STUW Double Double
