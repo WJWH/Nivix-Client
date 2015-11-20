3b770549a725297847a38460fd179c11d2495f2b
Wander Hillen <wjw.hillen@gmail.com>
2015-11-20 11:58:51 +0100

    adclezer.hs aangepast om de batterijspanning via een spanningsdeler te meten

--- Adclezer.hs
+++ Adclezer.hs
@@ -18,11 +18,15 @@ readBattery = do
     results <- replicateM 5 (threadDelay 10000 >> transferManySPI [1,144,0]) -- doe het vijf keer
     return . (myRound 2) . average . (map extractBatteryResults) $ results
     
+--de spanning van de batterij ligt ergens tussen drie en 4.2V?
+--de reference spanning van de ADC is maar 3.3V, dus een spanningsdeler van 2 10kOhm weerstanden haalt de spanning ver genoeg naar beneden om goed te meten.
+--je moet wel aan het eind de gemeten waarde weer met twee vermenigvuldigen om de waarde van de batterij ipv de waarde halverwege de spanningsdeler te krijgen
 extractBatteryResults :: (Bits a, Integral a) => [a] -> Double
-extractBatteryResults [_,p,q] = (adcresult/1023)*refvoltage
+extractBatteryResults [_,p,q] = 2 * adcresult * (refvoltage/1023)
     where   adcresult = ((fromIntegral (p .&. 0x3)) * 256) + (fromIntegral q)
-            refvoltage = 5.2
-    
+            refvoltage = 3.3 --de 3.3 rail van de pi
+
+--de formule komt van adafruit en wikipedia. 
 extractTemperatureResults :: (Bits a, Integral a) => [a] -> Double -- 
 extractTemperatureResults [_,p,q] = t - 273.15 -- terugrekenen van Kelvin naar Celsius
     where   t = 1/( (1/t0) + ((1/b)* (log (thermistorweerstand / r0)))) -- zie https://learn.adafruit.com/thermistor/using-a-thermistor
